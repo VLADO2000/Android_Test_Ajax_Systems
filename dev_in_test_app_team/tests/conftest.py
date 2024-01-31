37ca2e0eccf474c62b146ecd84d98939ab292f39
WLOT <vladlotov@ukr.net>
2024-01-31 11:58:20 +0200

    automatic appium server loading

--- dev_in_test_app_team/tests/conftest.py
+++ dev_in_test_app_team/tests/conftest.py
@@ -6,29 +6,33 @@ import time
 import pytest
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
+from appium.webdriver.appium_service import AppiumService
 
 from utils.android_utils import android_get_desired_capabilities
 
 
-# @pytest.fixture(scope='session')
-# def run_appium_server():
-#     subprocess.Popen(
-#         ['appium', '-a', '0.0.0.0', '-p', '4723'],
-#         stdout=subprocess.DEVNULL,
-#         stderr=subprocess.DEVNULL,
-#         stdin=subprocess.DEVNULL,
-#         shell=True
-#     )
-#     time.sleep(5)
+
+APPIUM_PORT = 4723
+APPIUM_HOST = '127.0.0.1'
+
+@pytest.fixture(scope='session')
+def appium_service():
+    service = AppiumService()
+    service.start(
+        args=['--address', APPIUM_HOST, '-p', str(APPIUM_PORT)],
+        timeout_ms=20000,
+    )
+    yield service
+    service.stop()
     
   
 
 
 @pytest.fixture(scope='session')
-def driver():
+def driver(appium_service):
     options = UiAutomator2Options()
     options.load_capabilities(android_get_desired_capabilities())
-    driver = webdriver.Remote('http://localhost:4723', options=options)
+    driver = webdriver.Remote(f'http://{APPIUM_HOST}:{APPIUM_PORT}', options=options)
     driver.implicitly_wait(5)
     yield driver
     driver.quit()
